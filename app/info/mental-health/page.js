e502f13e406f6a6642e8a710f0bfe302a78b2a6f
Giridhar Talla <giridhar.talla2002@gmail.com>
2023-11-29 09:23:38 +0530

    information pages added
    Co-authored-by: Dokyun Claire Lim <im.koremp@gmail.com>

---
+++ app/info/mental-health/page.js
@@ -0,0 +1,25 @@
+export default function MentalHealth() {
+  return (
+    <main className='flex w-full min-h-screen flex-col items-center justify-between py-24'>
+      <div className='container'>
+        <h1 className='text-3xl my-4'>정신 건강</h1>
+        <div className='container'>
+          <p className='text-xl'>자해</p>
+          <p className='text-xl'>우울</p>
+          <p className='text-xl'>지원</p>
+        </div>
+        <div>
+          informations about mental health
+
+          한국 사회에는 정신건강에 관한 부정적 선입견이 많습니다.
+
+          정신적 질병에 대해 나약해서 그렇다, 다들 그런 문제가 있다고 한다.
+
+          정신건강의학과가 주변에 존재한다.
+
+          정신건강 관련 지원 프로그램이 존재한다.
+        </div>
+      </div>
+    </main>
+  )
+}
---
+++ app/info/youth/osy/page.js
@@ -0,0 +1,26 @@
+export default function OSY() {
+  return (
+    <main className='flex max-w-full:sm min-h-screen flex-col items-center justify-between py-24'>
+      <div className='container'>
+        <h1 className='text-3xl my-4'>학교 밖 청소년</h1>
+        <div>
+          <p className='text-lg'>
+            학교 밖 청소년의 정의
+          </p>
+          <p className='text-lg'>
+            학교 밖 청소년 법률
+          </p>
+          <p className='text-lg'>
+            지원 정보
+          </p>
+          <p className='text-lg'>
+            고정관념, 오해와 진실
+          </p>
+          <p className='text-lg'>
+            관련 자료
+          </p>
+        </div>
+      </div>
+    </main>
+  )
+}
